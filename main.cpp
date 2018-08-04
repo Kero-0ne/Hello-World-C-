455b51f8d13b80ef1f9be341ac95207234396c28
Kero-0ne <42083427+Kero-0ne@users.noreply.github.com>
2018-08-04 00:24:24 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+main()
+{
+    printf("tu gfa");    system("cls");
+    printf("es puta xd");    system("cls");
+}
